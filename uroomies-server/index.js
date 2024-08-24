28ac62ce1c3ec22ae8221b21e8f6b353f80b06dd
Brandon <jbcraft88@gmail.com>
2024-08-24 09:05:32 -0700

    backend setup

--- uroomies-server/index.js
+++ uroomies-server/index.js
@@ -1,3 +1,13 @@
 import express from 'express'; 
+import dotenv from 'dotenv'; 
+
+dotenv.config();
+
+const PORT = process.env.PORT; 
+
 
 const app = express(); 
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+})
