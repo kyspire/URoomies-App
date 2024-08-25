e77e279772929af7d4bfbd59f74fd527611e6f6e
Brandon <jbcraft88@gmail.com>
2024-08-25 14:20:06 -0700

    added posgres connection

--- uroomies-server/index.js
+++ uroomies-server/index.js
@@ -1,13 +1,30 @@
 import express from 'express'; 
 import dotenv from 'dotenv'; 
+import pkg from 'pg'; 
+
 
 dotenv.config();
-
-const PORT = process.env.PORT; 
+//const PORT = process.env.PORT; 
+//const PASSWORD = process.env.PASSWORD;
+//env better, but it keeps crashing idk why
+const { Client } = pkg; 
+const client = new Client({
+  host: "localhost", 
+  user: "postgres", 
+  port: 5432, 
+  password: "password", 
+  database: "URoomies"
+})
 
 
 const app = express(); 
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-})
+client.connect()
+  .then(console.log("DB Connected!"))
+  .then(app.listen(7776, () => {
+    console.log(`Server is listening on port 7776`);
+  }))
+  .catch((err) => {
+    console.log(err);
+  })
+
