179f15bf3ac6b6bf7bf1f8595e63d1428704e649
kevinn-luu <kevvluu1234@gmail.com>
2024-08-26 19:39:39 -0700

    changed footer to a component

--- uroomies-client/src/pages/Landing.jsx
+++ uroomies-client/src/pages/Landing.jsx
@@ -1,9 +1,7 @@
 import "../styles/landing.css";
 import React from "react";
-import InstagramIcon from "../assets/InstagramIcon.svg";
-import TwitterIcon from "../assets/TwitterIcon.svg";
-import InformationIcon from "../assets/InformationIcon.svg";
 import { useNavigate } from "react-router-dom";
+import Footer from  "../components/BottomBar.jsx";
 
 function LandingBody() {
     return (
@@ -18,35 +16,11 @@ function LandingBody() {
     );
 }
 
-function LandingFooter() {
-    const navigate = useNavigate();
-
-    function handleClick() {
-        navigate("/information");
-    }
-
-    return (
-        <div className="footer">
-            <div className="footer-wrap">
-                <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="footer-btn">
-                    <img src={InstagramIcon} alt="Instagram" className="footer-icon" />
-                </a>
-                <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" className="footer-btn">
-                    <img src={TwitterIcon} alt="Twitter" className="footer-icon" />
-                </a>
-                <button className="footer-btn information" onClick={handleClick}>
-                    <img src={InformationIcon} alt="Information" className="footer-icon" />
-                </button>
-            </div>
-        </div>
-    );
-}
-
 function Landing() {
     return (
         <div className="landing-container">
             <LandingBody />
-            <LandingFooter />
+            <Footer />
         </div>
     );
 }
