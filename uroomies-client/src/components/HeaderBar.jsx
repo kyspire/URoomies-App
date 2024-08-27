c883f194cce7c67a3769bbb59ca34d7adeaee7f3
FriendlyKirby <uijinjung64@gmail.com>
2024-08-26 21:06:55 -0700

    skeleton for profile setup

---
+++ uroomies-client/src/components/HeaderBar.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { useNavigate } from "react-router-dom";
+import "../styles/Header.css";
+
+function Header() {
+    return(
+        <div>
+            
+        </div>
+    );
+};
+
+export default Header;
