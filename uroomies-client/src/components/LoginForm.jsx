a09f284e1b1be22b137e69c01e25c495daca6c41
kevinn-luu <kevvluu1234@gmail.com>
2024-08-26 21:32:11 -0700

    login page styling

--- uroomies-client/src/components/LoginForm.jsx
+++ uroomies-client/src/components/LoginForm.jsx
@@ -6,25 +6,28 @@ function LoginForm () {
   return (
     <>
       <div className='login-body'>
-        <form className='login-form'>
-          
-          <label htmlFor="email">Email:</label>
-          <input type='text' id='email' name='email' placeholder='Enter your email' required></input>
-          <br></br>
+        <img src="../../public/vannight3.jpg" alt="Background" className="background-image" />
+        <div className='form-wrapper'>
+          <form className='login-form'>
+            
+            <label htmlFor="email">Email:</label>
+            <input type='text' id='email' name='email' placeholder='Enter your email' required></input>
+            <br></br>
 
-          <label htmlFor="password">Password:</label>
-          <input type='text' id='password' name='password' placeholder='Enter your password' required></input>
-          <br></br>
+            <label htmlFor="password">Password:</label>
+            <input type='text' id='password' name='password' placeholder='Enter your password' required></input>
+            <br></br>
 
-          <label htmlFor="keep-signed-in">
-            <input type='checkbox' id='keep-signed-in' name='keep-signed-in' />
-            Keep me signed in 
-          </label>
-          <br></br>
+            <label htmlFor="keep-signed-in">
+              <input type='checkbox' id='keep-signed-in' name='keep-signed-in' />
+              Keep me signed in 
+            </label>
+            <br></br>
 
-          <input type='submit' value="Log in"></input>
+            <input type='submit' value="Log in"></input>
 
-        </form>
+          </form>
+        </div>
       </div>
     </>
     
