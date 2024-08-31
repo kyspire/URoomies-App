11f696b2d0ec9c3dc8c2370db1b87fd7d3b0eb99
Brandon <jbcraft88@gmail.com>
2024-08-31 16:39:34 -0700

    buggy chat but working

--- uroomies-server/index.js
+++ uroomies-server/index.js
@@ -47,6 +47,16 @@ const io = new Server(expressServer, {
 io.on('connection', socket => {
   console.log(`User ${socket.id} connected`);
 
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  })
+
+  socket.on("send_message", (data) => {
+    console.log(data);
+    socket.to(data.room).emit("receive_message", data);
+  })
+
 
   socket.on("disconnect", () => {
     console.log(`User ${socket.id} disconnected.`);
